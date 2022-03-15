967f82b2cb787b8d29ce5bed610a334c5d062834
Michael Lee <michael@mrlee.io>
2022-03-15 16:15:48 -0600

    Add BambooHR

--- ScoutSuite/providers/__init__.py
+++ ScoutSuite/providers/__init__.py
@@ -2,7 +2,8 @@ providers_dict = {'aws': 'AWSProvider',
                   'gcp': 'GCPProvider',
                   'azure': 'AzureProvider',
                   'aliyun': 'AliyunProvider',
-                  'oci': 'OracleProvider'}
+                  'oci': 'OracleProvider'
+                  'bamboohr': 'BambooHRProvider'}
 
 
 def get_provider_object(provider):
